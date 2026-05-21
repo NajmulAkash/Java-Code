8040ffbbb6d9b9cec74862e8491cde655fb1f99c
Najmul Akash <najmulbalghari23@gamil.com>
2026-05-21 06:36:31 -0700

    Set Time

---
+++ Settime.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+   console.log("1th Program") 
+}, 5000);
+setTimeout(() => {
+   console.log("2th Program") 
+}, 4000);
+setTimeout(() => {
+   console.log("3th Program") 
+}, 3000);
+setTimeout(() => {
+   console.log("4th Program") 
+}, 2000);
+setTimeout(() => {
+   console.log("5th Program") 
+}, 1000);
