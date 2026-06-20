0aec323288022f116991c8dca21f1f9809b450f6
Najmul Akash <najmulbalghari23@gamil.com>
2026-06-20 05:35:56 -0700

    task

---
+++ Even.js
@@ -0,0 +1,20 @@
+a=InputDeviceInfo
+)
+if (a=='a'){
+    console.log("Vowel")
+}
+else if(a=='e'){
+    console.log("Vowel")
+}
+else if(a=='i'){
+    console.log("Vowel")
+}
+else if(a=='o'){
+    console.log("Vowel")
+}
+else if(a=='u'){
+    console.log("Vowel")
+}
+else {
+console.log("Consunent")
+}
